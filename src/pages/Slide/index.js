edfdb01de1c0154a8e36dc747d1acb12be5cf310
yayakyogi <yayaktaka@gmail.com>
2021-05-14 09:11:33 +0700

    Ubah style color

--- src/pages/Slide/index.js
+++ src/pages/Slide/index.js
@@ -47,7 +47,7 @@ const styles = {
       flex: 1,
       justifyContent: 'center',
       alignItems: 'center',
-      backgroundColor: '#ffffff'
+      backgroundColor: '#ffffff',
     },
   },
   text:{
