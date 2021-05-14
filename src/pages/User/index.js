5feda0d8d0d89b7d02372b864c5ccc0cb6f323b4
yayakyogi <yayaktaka@gmail.com>
2021-05-15 02:06:41 +0700

    Component header selesai

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, ScrollView, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import Header from './dashboard/header';
+import {colors} from '../../util';
 
 const User = () => {
   return(
-    <Text>Halaman User</Text>
+    <ScrollView style={styles.dashboard}>
+      <Header/>
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  dashboard:{
+    backgroundColor: colors.white,
+  }
+});
+
 export default User;
