be33c51028be25ee7acf19a88514e02e9021e9d9
yayakyogi <yayaktaka@gmail.com>
2021-05-15 01:03:45 +0700

    File baru tempat menyimpan semua gambar yang digunakan berulang kali

---
+++ src/assets/img/index.js
@@ -0,0 +1,14 @@
+// folder brand
+import brand from './brand/brand.png'; // image brand UNNAR
+import logo from './brand/logo.png'; // image logo aplikasi
+import logoBulat from './brand/logo-bulat.png'; // image logo bulat UNNAR
+
+// folder auth
+import backButton from './auth/back-button.png'; // image tombol kembali
+import imgLogin from './auth/img-login.png'; // image halaman login
+import imgRegister from './auth/img-register.png'; // image halaman register
+
+export {
+    brand,logo,logoBulat,
+    backButton,imgLogin,imgRegister,
+};
