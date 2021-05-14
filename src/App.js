935aeb58318d9c2a5417c2236bc9bfa8e3c5b60a
yayakyogi <yayaktaka@gmail.com>
2021-05-15 01:14:55 +0700

    Hapus beberapa component yang tidak dipakai

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Router from './router';
+
 const App: () => Node = () => {
 return (
     <NavigationContainer>
@@ -18,9 +9,5 @@ return (
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  
-});
 
 export default App;
